3da8a1fb34a710c2a98886c9e17288879b4c748c
Kashi_Grobe <kashigrobe@icloud.com>
2023-11-06 01:07:32 +0100

    APi works

--- app.js
+++ app.js
@@ -12,15 +12,13 @@ app.use(bodyParser.json());
 
 // Serve static files
 // app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 
 // Initialize the database
-const database = db.initDb('./db', 'todolist.db');
-// db.initDb('./db', 'todolist.db');
+// const database = db.initDb('./db', 'todolist.db');
 
 // Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '', 'index.html'));
-});
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '', 'index.html'));});
 
 // Register a new email (No authentication)Crud
 app.post('/api/register', async (req, res) => {
@@ -42,6 +40,7 @@ app.post('/api/register', async (req, res) => {
 
 // Fetch tasks by email
 app.get('/api/getTasks', async (req, res) => {
+  console.log("Received request to fetch tasks for email:", req.query.email);
   try {
     const email = req.query.email; // Assume email is passed as a query parameter
     const tasks = await db.getTasksByUserEmail('./db', 'todolist.db', 'user@example.com');
@@ -52,17 +51,17 @@ app.get('/api/getTasks', async (req, res) => {
 });
 
 // Add a new task
-// Add a new task
-app.post('/api/addTask', async (req, res) => {
-  const { title, description, due_date } = req.body;
-
-  if (!title || !description || due_date === undefined) {  // <-- Updated this line
+app.get('/api/addTask', async (req, res) => {
+  // Using query parameters instead of body
+  const { title, description, due_date } = req.query;
+  
+  if (!title || !description || !due_date) {  // <-- Updated this line
     return res.status(400).json({ error: "All fields are required" });
   }
   
   try {
-    const email = "user@example.com";  // <-- Added this line
-    const completed = false;  // <-- Added this line
+    const email = "user@example.com";  // <-- The email is still hardcoded here
+    const completed = false;  // <-- Assuming the task is not completed when added
 
     await db.addNewTask('./db', 'todolist.db', email, title, description, due_date, completed);
     res.status(200).json({ message: "Task added successfully." });
@@ -72,25 +71,45 @@ app.post('/api/addTask', async (req, res) => {
 });
 
 
+
 // Update a task
-app.put('/api/updateTask/:id', async (req, res) => {
+app.get('/api/updateTask', async (req, res) => {
+  // Extract the task fields from the query string
+  const { id, title, description, due_date, completed } = req.query;
+  
+  // Check for the presence of the required fields
+  if (!id || !title || !description || !due_date || completed === undefined) {
+    return res.status(400).json({ error: "All task fields are required" });
+  }
+
+  // Convert completed to a boolean
+  const completedBool = completed === 'true';
+
   try {
-    await db.updateTask('./db', 'todolist.db', req.params.id, req.body);
+    // Call the updateTask function with the collected parameters
+    await db.updateTask('./db', 'todolist.db', id, title, description, due_date, completedBool);
     res.status(200).json({ message: "Task updated successfully." });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
+
 // Delete a task
-app.delete('/api/deleteTask/:id', async (req, res) => {
+app.get('/api/deleteTask', async (req, res) => {
+  const taskId = req.query.id; // Get the id from the query string
+  console.log("Received request to delete task with id:", taskId);
+  if (!taskId) {
+    return res.status(400).json({ error: "Task ID is required" });
+  }
   try {
-    await db.deleteTask('./db', 'todolist.db', req.params.id);
+    await db.deleteTask('./db', 'todolist.db', taskId);
     res.status(200).json({ message: "Task deleted successfully." });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
+
 
 // Start the server
 app.listen(port, () => {
